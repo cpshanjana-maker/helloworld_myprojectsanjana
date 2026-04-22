e6dbb28ae9d8ffec6be8ed855644762a1d104c42
sanjana <abc@gmail.com>
2026-04-22 21:35:20 +0530

    first commit

---
+++ HelloWorld.js
@@ -0,0 +1,8 @@
+const http = require("http");
+const server = http.createServer((req,res)=>{
+    res.end("My Hello World project");
+
+});
+server.listen(3000,() =>{
+    console.log("my application is running on port 3000");
+});
